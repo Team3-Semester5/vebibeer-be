c564e00ec4eeee53be99159255768aae27504d40
NhatTV <79240278+truongnhat2102@users.noreply.github.com>
2024-05-31 14:17:02 +0700

    update 31/5/2024

--- src/main/java/com/example/vebibeer_be/model/entities/BusCompany/Route.java
+++ src/main/java/com/example/vebibeer_be/model/entities/BusCompany/Route.java
@@ -3,6 +3,8 @@ package com.example.vebibeer_be.model.entities.BusCompany;
 import java.sql.Date;
 import java.util.Set;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -12,6 +14,7 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
